4e9fb85da75e74f6afcc42b8f475094af60932fd
Piyush Chauhan <chauhanpiyush@hotmail.com>
2017-12-14 21:33:00 +0530

    front end first commit

---
+++ front_end/JS_URL_Shortener.js
@@ -0,0 +1,11 @@
+function Shorten() {
+  /*
+  TODO: to shorten the url, make a server request
+  TODO: add the original and shortened url with the copy button to the table
+  TODO: change the button and its function
+  */
+}
+
+$("button").click(function show(){
+  $("#table").css("visibility":"show");
+});
