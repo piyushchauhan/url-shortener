8947e68d708e5074130868e696fef905baaa63c1
Piyush Chauhan <chauhanpiyush@hotmail.com>
2017-12-20 16:07:45 +0530

    appends to the tables

--- front_end/JS_URL_Shortener.js
+++ front_end/JS_URL_Shortener.js
@@ -1,11 +1,51 @@
-function Shorten() {
-  /*
-  TODO: to shorten the url, make a server request
-  TODO: add the original and shortened url with the copy button to the table
-  TODO: change the button and its function
-  */
-}
+// function Shorten() {
+//   /*
+//   TODO: to shorten the url, make a server request
+//   TODO: add the original and shortened url with the copy button to the table
+//   TODO: change the button and its function
+//   */
+// }
+function CopyFunction(n) {
 
-$("button").click(function show(){
-  $("#table").css("visibility":"show");
+
+$(document).ready(function() {
+  var numOfURLs = 0;
+  $("#table").hide();
+  $("#ShortCopy").click(function() {
+    //Function to copy the given URL and shortened URL in their respective column with the button to copy the short URL
+    numOfURLs += 1;
+    n = numOfURLs.toString();
+    var orgURL = $("input:text").val();
+    srtURL = function(orgURL) {
+
+      return;
+    };
+    $("input:text").val(srtURL);
+
+    $("tbody").prepend(
+      $("<tr></tr>").append(
+        $("<td></td>",{
+          'class':'orgURL',
+          'id':'orgURL'+n,
+          text:orgURL
+        }),
+        $("<td></td>",{
+          'class':'srtURL',
+          'id':'srtURL'+n,
+          text:srtURL
+        }).append(
+          $("<button>",{
+            'class':'copyButton',
+            'id':n;
+            text:'copy',
+            'onclick':function CopyFunction(n){
+              var copyText = document.getElementById("srtURL-" + n);
+              copyText.select();
+              document.execCommand("Copy");
+            };
+          })
+        );
+      );
+    );
+  });
 });
