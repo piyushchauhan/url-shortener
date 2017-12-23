2db7a1f412a30a66b60fd3be6498a0d814438dd7
Piyush Chauhan <chauhanpiyush@hotmail.com>
2017-12-23 11:39:34 +0530

    added clear button(working)

--- front_end/jq.js
+++ front_end/jq.js
@@ -1,9 +1,14 @@
-function shorten (orgURL) {//function that shortens the given URL
+function shorten(orgURL) { //function that shortens the given URL
   return "shrt " + orgURL;
 };
-function copy_from_input_box(){//funnction the
 
+function copy_from_input_box() { //funnction the
+  var copyText = document.getElementsByTagName("input");
+  copyText.select();
+  document.execCommand("Copy");
+  alert("Copied the text: " + copyText.value);
 };
+
 function returnAnchor(a) {
   return "<a href='" + a + "'>" + a + "</a>"
 };
@@ -11,29 +16,36 @@ function returnAnchor(a) {
 $(document).ready(function() {
   var numOfURLs = 0;
   $("#table").hide();
-  $("button.in").click(function() {
+  $("#short").click(function() {
     var orgURL = $("input").val();
     numOfURLs += 1;
     n = numOfURLs.toString();
     $("#table").show();
     $(this).html("Copy");
     var srtURL = shorten(orgURL);
-    // alert(srtURL);
-    $("input").val(srtURL);
+    $("input:text").val(srtURL);
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
     var td2 = document.createElement("td");
-    // var button = document.createElement("button")
-    // $(button).attr("id",n);
+    var button = document.createElement("button")
+    $(button).attr({
+      "id":n,
+      "class":"copy"
+    });
+    $(button).html("Copy");
     $(td1).addClass("orgURL");
     $(td2).addClass("srtURL");
-    $(td1).attr("id",n);
-    $(td2).attr("id",n);
-    // $(td2).addchlid(button);
+    $(td1).attr("id", n);
+    $(td2).attr("id", n);
+    $(td1).html(returnAnchor(orgURL));
+    $(td2).html(returnAnchor(srtURL));
+    $(button).appendTo(td2);
     $(td1).appendTo(tr);
     $(td2).appendTo(tr);
     $(tr).prependTo("tbody");
     this.id = "copy_from_input_box";
   });
-  // $("#copy").click();
+  $("#clear").click(function(){// Clears the input box
+    $("input:text").val("");
+  });
 });
